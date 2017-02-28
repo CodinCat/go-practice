255be88aa85644c4514ff8437ea117d69a5891ff
CodinCat <a55951234@gmail.com>
2017-02-28 23:59:38 +0800

    Add loop version of factorial

--- factorial.go
+++ factorial.go
@@ -6,3 +6,11 @@ func Factorial(n int) int {
 	}
 	return n * Factorial(n-1)
 }
+
+func FactorialLoop(n int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
--- factorial_test.go
+++ factorial_test.go
@@ -14,3 +14,12 @@ func TestFactorial(t *testing.T) {
 	assert.Equal(24, Factorial(4))
 	assert.Equal(120, Factorial(5))
 }
+
+func TestFactorialLoop(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(1, FactorialLoop(1))
+	assert.Equal(2, FactorialLoop(2))
+	assert.Equal(6, FactorialLoop(3))
+	assert.Equal(24, FactorialLoop(4))
+	assert.Equal(120, FactorialLoop(5))
+}
