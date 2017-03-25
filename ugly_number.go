4e8a1093cbbf6b112db65f8a36d99f4791ba046d
CodinCat <a55951234@gmail.com>
2017-03-25 19:43:13 +0800

    Ugly number

---
+++ ugly_number.go
@@ -0,0 +1,14 @@
+package practice
+
+func IsUglyNumber(n int) bool {
+	for n%2 == 0 {
+		n /= 2
+	}
+	for n%3 == 0 {
+		n /= 3
+	}
+	for n%5 == 0 {
+		n /= 5
+	}
+	return n == 1
+}
---
+++ ugly_number_test.go
@@ -0,0 +1,38 @@
+package practice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsUglyNumber(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(true, IsUglyNumber(1), "1")
+	assert.Equal(true, IsUglyNumber(2), "2")
+	assert.Equal(true, IsUglyNumber(3), "3")
+	assert.Equal(true, IsUglyNumber(4), "4")
+	assert.Equal(true, IsUglyNumber(5), "5")
+	assert.Equal(true, IsUglyNumber(6), "6")
+	assert.Equal(false, IsUglyNumber(7), "7")
+	assert.Equal(true, IsUglyNumber(8), "8")
+	assert.Equal(true, IsUglyNumber(9), "9")
+	assert.Equal(true, IsUglyNumber(10), "10")
+	assert.Equal(false, IsUglyNumber(11), "11")
+	assert.Equal(true, IsUglyNumber(12), "12")
+	assert.Equal(false, IsUglyNumber(13), "13")
+	assert.Equal(false, IsUglyNumber(14), "14")
+	assert.Equal(true, IsUglyNumber(15), "15")
+	assert.Equal(true, IsUglyNumber(16), "16")
+	assert.Equal(false, IsUglyNumber(17), "17")
+	assert.Equal(true, IsUglyNumber(18), "18")
+	assert.Equal(false, IsUglyNumber(19), "19")
+	assert.Equal(true, IsUglyNumber(20), "20")
+	assert.Equal(false, IsUglyNumber(21), "21")
+	assert.Equal(false, IsUglyNumber(22), "22")
+	assert.Equal(false, IsUglyNumber(23), "23")
+	assert.Equal(true, IsUglyNumber(24), "24")
+	assert.Equal(true, IsUglyNumber(25), "25")
+	assert.Equal(false, IsUglyNumber(26), "26")
+}
